ec675e5ea3bab17e381340cbf311bb2365e154fc
Arjuna Lanang <wardilanang46@gmail.com>
2023-12-27 20:25:30 +0700

    perubahan

---
+++ js/fe/script1.js
@@ -0,0 +1,8 @@
+function search(){
+    document.getElementById("1").style.display="none"
+    document.getElementById("2").style.display="none"
+    document.getElementById("3").style.display="none"
+    document.getElementById("4").style.display="none"
+    document.getElementById("5").style.display="none"
+    
+}
